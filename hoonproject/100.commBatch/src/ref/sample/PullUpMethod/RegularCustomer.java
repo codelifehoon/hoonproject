7f8cb9b607619802797dec6bb13452afc2b0b702
codelifehoon@gmail.com <codelifehoon@gmail.com@6549ca51-d6e5-44fb-5952-876d73d257e8>
2014-04-30 07:48:45 +0000

    리팩토링후

--- hoonproject/100.commBatch/src/ref/sample/PullUpMethod/RegularCustomer.java
+++ hoonproject/100.commBatch/src/ref/sample/PullUpMethod/RegularCustomer.java
@@ -2,18 +2,9 @@ package ref.sample.PullUpMethod;
 
 public class RegularCustomer extends Customer {
 
-		
-	public double createBill(int weight)
+	@Override	
+	protected double chargeFor(int weight)
 	{
-			double bill =  this.workingHours * chargeFor(weight);
-			System.out.println ("**************************");
-			 System.out.println (this.toString() + ":" +bill);
-			 System.out.println ("**************************");
-			return bill;
-	}
-	
-	private double chargeFor(int weight)
-	{
-		return weight * 1.5;
+		return weight * 0.5;
 	}
 }
