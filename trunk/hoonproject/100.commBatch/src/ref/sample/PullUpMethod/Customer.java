1aa755bc8ecc8b231069ba58e8cfb98f4cd489db
codelifehoon@gmail.com <codelifehoon@gmail.com@6549ca51-d6e5-44fb-5952-876d73d257e8>
2014-04-30 07:48:45 +0000

    리팩토링후

--- trunk/hoonproject/100.commBatch/src/ref/sample/PullUpMethod/Customer.java
+++ trunk/hoonproject/100.commBatch/src/ref/sample/PullUpMethod/Customer.java
@@ -2,12 +2,22 @@ package ref.sample.PullUpMethod;
 
 import java.util.Date;
 
-public class Customer {
+public abstract class Customer {
 	
 		protected int workingHours  =0 ;
 
 		public void addWorkingHours(int workingHours) {
 			this.workingHours = workingHours;
+		}
+
+		protected abstract double chargeFor(int weight);
+
+		public double createBill(int weight) {
+				double bill =  this.workingHours * chargeFor(weight);
+				 System.out.println ("**************************");
+				 System.out.println (this.toString() + ":" +bill);
+				 System.out.println ("**************************");
+				return bill;
 		}
 
 		
--- trunk/hoonproject/100.commBatch/src/ref/sample/PullUpMethod/PreferredCustomer.java
+++ trunk/hoonproject/100.commBatch/src/ref/sample/PullUpMethod/PreferredCustomer.java
@@ -3,18 +3,10 @@ package ref.sample.PullUpMethod;
 public class PreferredCustomer extends Customer {
 
 	
-	public double createBill(int weight)
+	@Override
+	protected double chargeFor(int weight)
 	{
-			double bill =  this.workingHours * chargeFor(weight);
-			 System.out.println ("**************************");
-			 System.out.println (this.toString() + ":" +bill);
-			 System.out.println ("**************************");
-			return bill;
-	}
-	
-	private double chargeFor(int weight)
-	{
-		return weight * 0.5;
+		return weight * 1.5;
 	}
 		
 }
