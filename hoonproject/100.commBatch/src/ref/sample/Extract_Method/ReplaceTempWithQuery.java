25e5fda86c22ae2794e888251499834c0a51abdc
codelifehoon@gmail.com <codelifehoon@gmail.com@6549ca51-d6e5-44fb-5952-876d73d257e8>
2014-04-30 06:15:42 +0000

    리팩토링전

---
+++ hoonproject/100.commBatch/src/ref/sample/Extract_Method/ReplaceTempWithQuery.java
@@ -0,0 +1,34 @@
+package ref.sample.Extract_Method;
+
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Vector;
+
+public class ReplaceTempWithQuery {
+
+	private String _name  = "#name#";
+	
+	 public void printOwing(Vector<OrderBO>  _orders) 
+	{
+	    Enumeration<OrderBO> e =_orders.elements();
+	    double outstanding = 0.0;
+	    double tempVal = 123*123;
+	    
+	 
+	    // print banner
+	    System.out.println ("**************************");
+	    System.out.println ("***** Customer Owes ******");
+	    System.out.println ("**************************");
+	 
+	    // calculate outstanding
+	    while (e.hasMoreElements()) {
+	    	OrderBO each = (OrderBO) e.nextElement();
+	        outstanding += each.getAmount() * tempVal;
+	    }
+	 
+	    //print details
+	    System.out.println ("name:" + _name);
+	    System.out.println ("amount" + outstanding);
+	}
+	
+}
