0e82bdb4775da6fe1c00ee92b2416458d2f1295d
codelifehoon@gmail.com <codelifehoon@gmail.com@6549ca51-d6e5-44fb-5952-876d73d257e8>
2014-04-30 08:29:05 +0000

    리팩토링후

---
+++ trunk/hoonproject/100.commBatch/src/ref/sample/FormTemplateMethod/Statement.java
@@ -0,0 +1,43 @@
+package ref.sample.FormTemplateMethod;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+public abstract class Statement {
+
+	protected Vector<Rental> vRentals;
+
+	public Statement() {
+		super();
+	}
+
+	public void setvRentals(Vector<Rental> vRentals) {
+		this.vRentals = vRentals;
+	}
+
+	protected abstract String getHeader();
+
+	protected abstract String getFooter(String result);
+
+	protected abstract String getBody(String result, Rental each);
+
+	public String statement() {
+	    Enumeration<Rental> rentals = vRentals.elements();
+	
+	    String result = getHeader();
+	    while (rentals.hasMoreElements()) {
+	        Rental each = (Rental) rentals.nextElement();
+	        //show figures for each rental
+	        result = getBody(result, each);
+	    }
+	    //add footer lines
+	    result = getFooter(result);
+	
+	
+	
+	    return result;
+	}
+
+
+
+}
