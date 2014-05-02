c2b0b9c203877c68f7ffc1f581324027b349adac
codelifehoon@gmail.com <codelifehoon@gmail.com@6549ca51-d6e5-44fb-5952-876d73d257e8>
2014-05-02 04:51:10 +0000

    리팩토링후

---
+++ hoonproject/100.commBatch/src/ref/sample/Extract_Method/OrderBO.java
@@ -0,0 +1,21 @@
+package ref.sample.Extract_Method;
+
+public class OrderBO {
+
+		int amount;
+
+		public OrderBO(int amount )
+		{
+			this.amount = amount;
+		}
+		
+		public int getAmount() {
+			return amount;
+		}
+
+		public void setAmount(int amount) {
+			this.amount = amount;
+		}
+		
+		
+}
