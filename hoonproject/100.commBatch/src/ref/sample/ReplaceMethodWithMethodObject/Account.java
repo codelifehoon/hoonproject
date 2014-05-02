07fb517b6b4034bdfa701e7101d1675789b81b36
codelifehoon@gmail.com <codelifehoon@gmail.com@6549ca51-d6e5-44fb-5952-876d73d257e8>
2014-05-02 08:52:07 +0000

    리팩토링후

--- hoonproject/100.commBatch/src/ref/sample/ReplaceMethodWithMethodObject/Account.java
+++ hoonproject/100.commBatch/src/ref/sample/ReplaceMethodWithMethodObject/Account.java
@@ -5,17 +5,10 @@ public class Account {
 
 	public int gamma (int inputVal, int quantity, int yearToDate) 
 	{
-	    int importantValue1 = (inputVal * quantity) + delta();
-	    int importantValue2 = (inputVal * yearToDate) + 100;
-	    if ((yearToDate - importantValue1) > 100)
-	        importantValue2 -= 20;
-	    int importantValue3 = importantValue2 * 7;
-	    
-	    // and so on...
-	    return importantValue3 - 2 * importantValue1;
+		return new Gamma.
 	}
 
-	private int delta()
+	public int delta()
 	{
 		return 13*5;
 	}
