a02eb9cf419f1d59301018de088ff94938984971
codelifehoon@gmail.com <codelifehoon@gmail.com@6549ca51-d6e5-44fb-5952-876d73d257e8>
2014-05-07 12:06:19 +0000

    리팩토링후

--- trunk/hoonproject/100.commBatch/src/ref/sample/MoveField/AccountType.java
+++ trunk/hoonproject/100.commBatch/src/ref/sample/MoveField/AccountType.java
@@ -2,4 +2,13 @@ package ref.sample.MoveField;
 
 public class AccountType {
 	private double kind;
+	private double interestRate  = 10.0;
+	
+	public double getInterestRate() {
+		return interestRate;
+	}
+	public void setInterestRate(double interestRate) {
+		this.interestRate = interestRate;
+	}
+
 }
