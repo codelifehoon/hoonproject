b196450b307c1321a71f770d7c30d4625fdc0b67
codelifehoon@gmail.com <codelifehoon@gmail.com@6549ca51-d6e5-44fb-5952-876d73d257e8>
2014-05-07 05:39:51 +0000

    리팩토링후

--- trunk/hoonproject/100.commBatch/src/ref/sample/ExtractSubclass/CalPay.java
+++ trunk/hoonproject/100.commBatch/src/ref/sample/ExtractSubclass/CalPay.java
@@ -2,31 +2,20 @@ package ref.sample.ExtractSubclass;
 
 public class CalPay {
 
-	private int pay;
-    private int workingDay;
-    private boolean speicalIm;
+	protected int pay;
+	protected int workingDay;
+
     
-	public CalPay(int pay, int workingDay, boolean speicalIm) {
+	public CalPay(int pay, int workingDay) {
 		super();
 		this.pay = pay;
 		this.workingDay = workingDay;
-		this.speicalIm = speicalIm;
 	}
 	
 	public int doCalc()
 	{
-		int calcPay=0;
-		if (this.speicalIm)
-		{
-			calcPay = pay * workingDay * 2;
-		}
-		else
-		{
-			calcPay = pay * workingDay ;
-		}
 		
-		return calcPay;
-		
+		return	 pay * workingDay ;
 		
 	}
     
