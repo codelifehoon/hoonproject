065586002e83eb9ef56d1a1dd706d9e0c43dfad0
codelifehoon@gmail.com <codelifehoon@gmail.com@6549ca51-d6e5-44fb-5952-876d73d257e8>
2014-05-08 05:34:53 +0000

    리팩토링후

--- trunk/hoonproject/100.commBatch/src/ref/sample/ExtractClass/TelephoneNumber.java
+++ trunk/hoonproject/100.commBatch/src/ref/sample/ExtractClass/TelephoneNumber.java
@@ -1,3 +1,12 @@
+/**
+ * @FileName  : Person.java
+ * @Project     : code refactoring exam proj
+ * @Date         : 2014. 5. 8. 
+ * @작성자      : codelife
+ * @변경이력 :
+ * @프로그램 설명 :
+ */
+
 package ref.sample.ExtractClass;
 
 public class TelephoneNumber {
