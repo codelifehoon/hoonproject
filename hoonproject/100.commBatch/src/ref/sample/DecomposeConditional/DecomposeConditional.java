a0063a9abd27148599952b04aad78e2a2cf293d2
codelifehoon@gmail.com <codelifehoon@gmail.com@6549ca51-d6e5-44fb-5952-876d73d257e8>
2014-05-08 05:34:53 +0000

    리팩토링후

--- hoonproject/100.commBatch/src/ref/sample/DecomposeConditional/DecomposeConditional.java
+++ hoonproject/100.commBatch/src/ref/sample/DecomposeConditional/DecomposeConditional.java
@@ -1,3 +1,11 @@
+/**
+ * @FileName  : Person.java
+ * @Project     : code refactoring exam proj
+ * @Date         : 2014. 5. 8. 
+ * @작성자      : codelife
+ * @변경이력 :
+ * @프로그램 설명 :
+ */
 package ref.sample.DecomposeConditional;
 
 public class DecomposeConditional {
