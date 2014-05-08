abfd79b79a784c06a02f12d092c6001117563872
codelifehoon@gmail.com <codelifehoon@gmail.com@6549ca51-d6e5-44fb-5952-876d73d257e8>
2014-05-08 05:14:13 +0000

    리팩토링후

--- hoonproject/100.commBatch/src/ref/sample/InlineClass/TelephoneNumber.java
+++ hoonproject/100.commBatch/src/ref/sample/InlineClass/TelephoneNumber.java
@@ -1,24 +1,7 @@
 package ref.sample.InlineClass;
 
 public class TelephoneNumber {
-    private String number;
-    private String areaCode;
-    
-	public String getTelephoneNumber() {
-        return ("(" + areaCode + ") " + number);
-    }
-	public String getAreaCode() {
-        return areaCode;
-    }
-	public void setAreaCode(String arg) {
-        areaCode = arg;
-    }
-	public String getNumber() {
-        return number;
-    }
-	public void setNumber(String arg) {
-        number = arg;
-    }
+
 
     
 }
