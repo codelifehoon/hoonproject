13c567dba211024c83b4faf5a42f1216ee027994
codelifehoon@gmail.com <codelifehoon@gmail.com@6549ca51-d6e5-44fb-5952-876d73d257e8>
2014-05-08 05:08:41 +0000

    리팩토링후

---
+++ trunk/hoonproject/100.commBatch/src/ref/sample/ExtractInterface/ExtractInterface.java
@@ -0,0 +1,11 @@
+package ref.sample.ExtractInterface;
+
+import java.util.Vector;
+
+import ref.sample.Extract_Method.OrderBO;
+
+public interface ExtractInterface {
+
+	public void printOwing(Vector<OrderBO> _orders);
+
+}
