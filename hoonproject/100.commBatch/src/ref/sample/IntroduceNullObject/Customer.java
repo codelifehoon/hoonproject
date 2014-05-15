db8e860a87f186cf69c941b5265d8ec6173e5947
codelifehoon@gmail.com <codelifehoon@gmail.com@6549ca51-d6e5-44fb-5952-876d73d257e8>
2014-05-15 11:32:07 +0000

    리팩토링전

---
+++ hoonproject/100.commBatch/src/ref/sample/IntroduceNullObject/Customer.java
@@ -0,0 +1,36 @@
+/**
+ * @FileName  : Customer.java
+ * @Project     : code refactoring exam proj
+ * @Date         : 2014. 5. 15. 
+ * @작성자      : codelife
+ * @변경이력 :
+ * @프로그램 설명 :
+ */
+package ref.sample.IntroduceNullObject;
+
+public class Customer {
+
+	private String name;
+	private String age;
+	private String gender;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+	
+}
