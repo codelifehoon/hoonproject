3b31673567f557429c3e61f27276d5fc450f7418
codelifehoon@gmail.com <codelifehoon@gmail.com@6549ca51-d6e5-44fb-5952-876d73d257e8>
2014-05-15 06:59:31 +0000

    리팩토링전

---
+++ hoonproject/100.commBatch/src/ref/sample/ReplaceConditionalWithPolymorphism/EmployeeType.java
@@ -0,0 +1,31 @@
+/**
+ * @FileName  : EmployeeType.java
+ * @Project     : code refactoring exam proj
+ * @Date         : 2014. 5. 13. 
+ * @작성자      : codelife
+ * @변경이력 :
+ * @프로그램 설명 :
+ */
+package ref.sample.ReplaceConditionalWithPolymorphism;
+
+public abstract class EmployeeType {
+
+    public static final int ENGINEER = 0;
+    public static final int SALESMAN = 1;
+    public static final int MANAGER = 2;
+    
+    static EmployeeType newType(int code) {
+        switch (code) {
+            case ENGINEER:
+               return new Engineer();
+            case SALESMAN:
+               return new Salesman();
+            case MANAGER:
+               return new Manager();
+            default:
+               throw new IllegalArgumentException("Incorrect Employee Code");
+        }
+    }
+
+    abstract int getTypeCode();
+  }
