c463788817427a363d1f0c9213b4ad3a5e648157
codelifehoon@gmail.com <codelifehoon@gmail.com@6549ca51-d6e5-44fb-5952-876d73d257e8>
2014-05-16 07:08:34 +0000

    리팩토링전

---
+++ trunk/hoonproject/100.commBatch/src/ref/sample/ReplaceDelegationWithInheritance/Person.java
@@ -0,0 +1,44 @@
+/**
+ * @FileName  : Person.java
+ * @Project     : code refactoring exam proj
+ * @Date         : 2014. 5. 8. 
+ * @작성자      : codelife
+ * @변경이력 :
+ * @프로그램 설명 :
+ */
+
+
+package ref.sample.ReplaceDelegationWithInheritance;
+
+public class Person {
+
+	private String _name;
+    private TelephoneNumber data = new TelephoneNumber();
+
+
+	public void setName(String _name) {
+        this._name = _name;
+    }
+    
+	public String getName() {
+        return _name;
+    }
+    public String getTelephoneNumber() {
+        return ("(" + data._officeAreaCode + ") " + data._officeNumber);
+    }
+    public String getOfficeAreaCode() {
+        return data._officeAreaCode;
+    }
+    public void setOfficeAreaCode(String arg) {
+        data._officeAreaCode = arg;
+    }
+    public String getOfficeNumber() {
+        return data._officeNumber;
+    }
+    public void setOfficeNumber(String arg) {
+        data._officeNumber = arg;
+    }
+    
+
+    
+}
