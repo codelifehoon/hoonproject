cc83c6f89172dc3e71a040d3e3b2e27d23f131a9
codelifehoon@gmail.com <codelifehoon@gmail.com@6549ca51-d6e5-44fb-5952-876d73d257e8>
2014-05-21 05:52:34 +0000

    리팩토링전

---
+++ hoonproject/100.commBatch/src/ref/sample/ChangeBidirectionalAssociationToUnidirectional/Order.java
@@ -0,0 +1,43 @@
+/**
+ * @FileName  : Order.java
+ * @Project     : code refactoring exam proj
+ * @Date         : 2014. 5. 19. 
+ * @작성자      : codelife
+ * @변경이력 :
+ * @프로그램 설명 :
+ */
+package ref.sample.ChangeBidirectionalAssociationToUnidirectional;
+
+public class Order {
+
+	private String name;
+	private String tel;
+	
+		
+	Customer getCustomer() {
+        return _customer;
+    }
+    void setCustomer (Customer arg) {
+        if (_customer != null) _customer.friendOrders().remove(this);
+        _customer = arg;
+        if (_customer != null) _customer.friendOrders().add(this);
+    }
+    private Customer _customer;
+
+
+    
+    
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+    
+}
