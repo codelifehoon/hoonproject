655882701f02169f1f539b42e5b7b9bd44d6c0bd
codelifehoon@gmail.com <codelifehoon@gmail.com@6549ca51-d6e5-44fb-5952-876d73d257e8>
2014-05-22 10:05:53 +0000

    리팩토링후

--- trunk/hoonproject/100.commBatch/src/ref/sample/EncapsulateCollection/Person.java
+++ trunk/hoonproject/100.commBatch/src/ref/sample/EncapsulateCollection/Person.java
@@ -8,18 +8,42 @@
  */
 package ref.sample.EncapsulateCollection;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
+
+import org.springframework.util.Assert;
 
 public class Person {
 
-	private Set  _courses;
+	private Set  _courses= new HashSet<>();
  	
 	public Set getCourses() {
-        return _courses;
+        return Collections.unmodifiableSet(_courses);
     }
-    public void setCourses(Set arg) {
-        _courses = arg;
+    public void initializeCourses(Set arg) {
+    	 Assert.isTrue(_courses.isEmpty());
+    	 _courses.addAll(arg);
     }
     
+    public void addCourse (Course arg) {
+        _courses.add(arg);
+    }
+    public void removeCourse (Course arg) {
+        _courses.remove(arg);
+    }
+	/**
+	 * @return
+	 */
+	public int numberOfAdvancedCourses() {
+		Iterator iter = getCourses().iterator();
+	    int count = 0;
+	    while (iter.hasNext()) {
+	    	ref.sample.EncapsulateCollection.Course each = (ref.sample.EncapsulateCollection.Course) iter.next();
+	       if (each.isAdvanced()) count ++;
+	    }
+		return count;
+	}
     
 }
