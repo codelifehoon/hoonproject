d4bfd50809c1c7542572216d12714738e5ad985c
codelifehoon@gmail.com <codelifehoon@gmail.com@6549ca51-d6e5-44fb-5952-876d73d257e8>
2014-05-22 06:08:10 +0000

    리팩토링전

---
+++ trunk/hoonproject/100.commBatch/src/ref/sample/IntroduceLocalExtension/IntroduceLocalExtension.java
@@ -0,0 +1,29 @@
+/**
+ * @FileName  : IntroduceForeignMethod.java
+ * @Project     : code refactoring exam proj
+ * @Date         : 2014. 5. 22. 
+ * @작성자      : codelife
+ * @변경이력 :
+ * @프로그램 설명 :
+ */
+package ref.sample.IntroduceLocalExtension;
+
+import java.util.Date;
+
+public class IntroduceLocalExtension {
+
+	public Date newStart(Date previousEnd )
+	{
+		 Date newStart = NextDay(previousEnd);
+		 return newStart;
+	}
+
+	/**
+	 * @param previousEnd
+	 * @return
+	 */
+	private static Date NextDay(Date previousEnd) {
+		return new Date (previousEnd.getYear(),
+                 previousEnd.getMonth(), previousEnd.getDate() + 1);
+	}
+}
